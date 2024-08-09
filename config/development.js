59061eb34d6f3b2bd8b717c32b64afd759e0b763
Ravi Kumar <ravi.kumar@rapidops.com>
2024-08-09 14:30:47 +0530

    fixed in config file

--- config/development.js
+++ config/development.js
@@ -4,7 +4,7 @@ const config = {
     mysql : {
         host: process.env.HOST,
         port: process.env.PORT,
-        user: process.env.USERNAME,
+        user: process.env.NAME,
         password: process.env.PASSWORD,
         database: process.env.DATABASE,
         poolSize: 20,
